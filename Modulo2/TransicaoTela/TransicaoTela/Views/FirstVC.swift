bbeaa1589a468148662e3b1ce8f2da6d21216aca
felipe silva <felipe.instrutor.ios@gmail.com>
2020-09-28 19:20:48 -0300

    implementei a mudança de tela via code

--- Modulo2/TransicaoTela/TransicaoTela/Views/FirstVC.swift
+++ Modulo2/TransicaoTela/TransicaoTela/Views/FirstVC.swift
@@ -12,6 +12,8 @@ class FirstVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         print("viewDidLoad first")
         // Do any additional setup after loading the view.
     }
@@ -29,6 +31,32 @@ class FirstVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         print("viewWillAppear first")
     }
+    
+   
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//
+//        let vc: SecondVC? =  storyboard.instantiateViewController(identifier: "SecondVC") as? SecondVC
+//
+//
+//
+//        self.present(vc ?? UIViewController(), animated: true, completion: nil)
 
+       
+        self.performSegue(withIdentifier: "DetailVC", sender: "Felipe")
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        let vc: DetailVC? = segue.destination as? DetailVC
+        vc?.view.backgroundColor = .red
+        vc?.myLabel.text = sender as? String
+        
+    }
+
+    
 }
 
